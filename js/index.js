331554dae761a4b4381a7f41587fba4dabf97cb9
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-24 16:41:59 +0100

    fixed

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
 /*
-Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1:
-Trasformare la stringa foto in una immagine effettiva
-BONUS 2:
-Organizzare i singoli membri in card/schede
+Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+BONUS
+1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+2- popolare le options della select della milestone 3 dinamicamente.
 */
 
-//creo array di oggetti con tutti i membri del team
+//creo array di oggetti con tutte le icone
 const icons = [
     {
         name: 'cat',
@@ -128,27 +125,55 @@ const icons = [
         color: 'blue'
     }
 ];
+function creaCards (oggetto) {
+    //creiamo una costante per prendere la row
+    const row = document.querySelector('.row');
+    row.innerHTML ='';
+    let cards = '';
+    //ciclo per creare delle card per ogni oggetto nell'array e i loro rispettivi valori
+    for (let i = 0; i < icons.length; i++) {
+        const icona = icons[i];
+        //constante card con il contenuto da creare e inserire 
+        const card = `
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="card-body d-flex flex-column justify-content-center align-items-center fs-3 py-3" style="color:${icona.color}">
+                    <i class="${icona.prefix}${icona.family} ${icona.prefix}${icona.name}"></i>
+                    <span class="text-black">${icona.name}</span>
+                </div>
+            </div>
+        </div>
+        `
+        //do il valore di card alla variabile cards
+        cards += card;
+    }
+    //inserisco tutte le card nella row
+    row.innerHTML += cards;
+};
 
- //creiamo una costante per prendere la row
-const row = document.querySelector('.row');
 
-let cards = '';
- //ciclo per creare delle card per ogni oggetto nell'array e i loro rispettivi valori
-for (let i = 0; i < icons.length; i++) {
-      const icona = icons[i];
-       //constante card con il contenuto da creare e inserire 
-      const card = `
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-          <div class="card">
-              <div class="card-body d-flex flex-column justify-content-center align-items-center fs-3 py-3" style="color:${icona.color}">
-                   <i class="${icona.prefix} ${icona.family} ${icona.prefix} ${icona.name}"></i>
-                   <span>${icona.name}</span>
-              </div>
-          </div>
-      </div>
-      `
-     //do il valore di card alla variabile cards
-     cards += card;
+function disegnaIcone(selezionaValore) {
+   const arrayFiltrato = icons.filter((value) =>{
+    if (value.type === selezionaValore || selezionaValore === 'all'){
+        return true;
+    } else {
+        return false;
+    }
+   })
+    // se l'elemento ha superato il test creo le card
+    arrayFiltrato.forEach((value) => {
+        creaCards(value)
+    })
+    
+};
+
+
+
+
+function selezionaLivello () {
+     disegnaIcone(this.value)
 }
- //inserisco tutte le card nella row
-row.innerHTML += cards;
+
+const select = document.getElementById('select')
+select.addEventListener('change', selezionaLivello)
+disegnaIcone(select.value);
